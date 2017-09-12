00c8170c4e6f75998ef4796448de103263a5049d
Jirka Vrba <jirkavrbis11@gmail.com>
2017-09-12 12:45:25 +0200

    frame

---
+++ www/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+	<meta charset="UTF-8">
+	<title>Věra Bělinová &mdash; Autorské šperky z PET lahví</title>
+
+	<script src='https://code.jquery.com/jquery-3.2.1.min.js'></script>
+	<link rel="stylesheet" href="css/preloader.css">
+	<script src='js/preloader.js'></script>
+</head>
+<body>
+	<div id="preloader">
+	  <div id="status">&nbsp;</div>
+	</div>
+	<div id="page">
+		<header>
+			<section class="main-header">
+				<h1 class='nadpis'>Autorské šperky z recyklovaných materiálů</h1>
+				<h2>Věra Bělinová</h2>
+				<img src="../podklady/foto/uvodni-fotka.jpg" alt="">
+			</section>
+		</header>
+	</div>
+</body>
+</html>
